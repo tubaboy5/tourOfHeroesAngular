b6393ee46e69671bc53a6daba1786ff65e964a2f
Justin Hartmann <hartmann.justin@gmail.com>
2021-01-20 07:32:06 -0600

    Add ability to select a hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,10 +11,10 @@ export class HeroesComponent implements OnInit {
 
 heroes = HEROES;
 
-    hero: Hero = {
-      id: 1,
-      name: 'Windstorm'
-    };
+selectedHero: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 
   constructor() { }
 
