a7e98a388ba0870090e3855a3a767e6da4191274
Justin Hartmann <hartmann.justin@gmail.com>
2021-01-30 16:14:28 -0600

    Final code cleanup of hero.service.ts

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
+  constructor(private messageService: MessageService) {}
+
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
@@ -17,8 +19,6 @@ export class HeroService {
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    return of(HEROES.find((hero) => hero.id === id));
   }
-
-  constructor(private messageService: MessageService) {}
 }
